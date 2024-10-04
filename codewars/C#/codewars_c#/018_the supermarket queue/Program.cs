364696ed92629916704a39238490954d00786aae
borics krisztián <80693491+boricsk@users.noreply.github.com>
2024-10-04 16:07:03 +0200

    241004

---
+++ codewars/C#/codewars_c#/018_the supermarket queue/Program.cs
@@ -0,0 +1,70 @@
+﻿/*
+A szupermarketben sorba kell állni az önpénztári pénztáraknál. Az Ön feladata, 
+hogy írjon egy függvényt, amely kiszámítja az összes ügyfél kijelentkezési idejét!
+
+bemenet
+ügyfelek: pozitív egész számok tömbje, amely a sort reprezentálja. Minden egész 
+szám egy ügyfelet jelöl, értéke pedig a kijelentkezéshez szükséges idő.
+n: pozitív egész szám, a pénztárak száma.
+
+kimenet
+A függvénynek egy egész számot kell visszaadnia, vagyis a szükséges teljes időt.
+
+Példák
+queueTime([5;3;4];1)
+// 12-et kell visszaadnia
+// mert amikor 1 meddig van, a teljes idő csak az idők összege
+
+queueTime([10;2;3;3];2)
+// 10-et kell visszaadnia, mert itt n=2, és a sorban lévő 2., 3. és 4. ember előbb végez, mint az 1. személy.
+
+queueTime([2;3;10];2)
+// 12-et kell visszaadnia
+
+Pontopontosítások
+Csak EGY sorban áll sok kassza kiszolgálására, és A sor sorrendje SOHA nem változik, és
+A sorban álló első személy (azaz a tömb/lista első eleme) a kasszához lép, amint felszabadul.
+N.B. Fel kell tételeznie, hogy az összes tesztbevitel érvényes lesz, a fent leírtak szerint.
+
+P.S. A helyzet ebben a katában hasonlítható a számítógép-tudományokkal kapcsolatos 
+szálkészlet ötletéhez, több folyamat egyidejű futtatásához: https://en.wikipedia.org/wiki/Thread_pool
+ */
+
+namespace _018_the_supermarket_queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test1 = { 5, 3, 4 };
+            int[] test2 = { 10, 2, 3, 3 };
+            int[] test3 = { 2, 3, 10 };
+
+            Console.WriteLine(QueueTime(test1, 1));
+            Console.WriteLine(QueueTime(test2, 2));
+            Console.WriteLine(QueueTime(test3, 100));
+
+        }
+        public static long QueueTime(int[] customers, int n)
+        {
+            long ret = 0;
+            int[] self_checkouts = new int[n];
+
+            if (n == 1) { ret = customers.Sum(); }
+            else if (customers.Length == 0 || customers == null) { ret = 0; }
+            else if (n >= customers.Length) { ret = customers.Max(); }
+            else
+            {
+                foreach (int customer in customers)
+                {
+                    self_checkouts[0] += customer;
+                    Array.Sort(self_checkouts);
+                }
+                ret = self_checkouts.Max();
+            }
+
+            return ret;
+        }
+
+    }
+}
---
+++ codewars/C#/codewars_c#/019_count smiley/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Adott egy tömb (arr) argumentumként fejezze be a countSmileys függvényt, amelynek vissza kell adnia a mosolygó arcok számát.
+
+A mosolygó arc szabályai:
+
+Minden mosolygós arcnak érvényes szempárt kell tartalmaznia. A szemek a következőképpen jelölhetők: vagy ;
+A mosolygós arcnak lehet orra, de nem kell. Az orr érvényes karakterei a - vagy a ~
+Minden mosolygó arcnak mosolygós szájjal kell rendelkeznie, amelyet ) vagy D betűvel kell megjelölni
+További karakterek nem megengedettek, kivéve az említetteket.
+
+Érvényes mosolygós példák: :) :D ;-D :~)
+Érvénytelen mosolygó arcok: ;( :> :} :]
+
+Példa 
+countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+*/
+namespace _019_count_smiley
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tests = { ":)", ";(", ";}", ":-D" };
+            Console.WriteLine(CountSmileys(tests));
+        }
+        public static int CountSmileys(string[] smileys)
+        {
+            if (smileys.Count() == 0 || smileys == null) { return 0; }
+            int count = 0;
+            string[] valid_smiles = { ":)", ":-)", ":~)", ":D", ":-D", ":~D", ";)", ";-)", ";~)", ";D", ";-D", ";~D" };
+
+            foreach (string smiley in smileys)
+            {
+                if (valid_smiles.Contains(smiley)) { count++; }
+            }
+            return count;
+        }
+    }
+}
