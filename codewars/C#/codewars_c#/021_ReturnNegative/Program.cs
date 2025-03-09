1c3dabe44be63479e10b1d1e26cf23085766bfea
boricsk <borics.krisztian@gmail.com>
2025-03-09 18:22:31 +0100

    250309

---
+++ codewars/C#/codewars_c#/021_ReturnNegative/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _021_ReturnNegative
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MakeNegative(-3));
+        }
+
+        public static int MakeNegative(int number)
+        {            
+            //return int.IsNegative(number) ? number : number * -1;
+            //Negálunk minden bitet és 1-et hozzáadunk.
+            return int.IsNegative(number) ? number : ~number + 1;
+        }
+    }
+}
---
+++ codewars/C#/codewars_c#/022_Digitizer/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _022_Digitizer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Digitize(3654);
+        }
+
+        public static long[] Digitize(long n)
+        {
+            long[] ns = n.ToString().Select(c => (long)(c - '0')).Reverse().ToArray();
+            return ns;
+        }
+    }
+}
